86e7e48d482e3382e64d2b8242ac7172899d32be
Hofer, Gabriel <gabriel.hofer@mines.sdsmt.edu>
2021-04-01 11:38:51 -0600

    getting the path using dp

--- gwrl.py
+++ gwrl.py
@@ -14,66 +14,92 @@ class GWRL:
 
     def train(self):
         """ Update grid values based on the policy  """
-        temp = np.zeros([self.rows,self.cols], dtype=float)
+        temp_grid = np.zeros([self.rows,self.cols], dtype=float)
         for i in range(self.rows):
             for j in range(self.cols):
                 if (not (i==0 and j==0)) and (not (i==self.rows-1 and j==self.cols-1)):
-                    temp[i,j] += self.policy[0] * (self.reward + \
-                        self.gamma * self.grid[ (i+1 if i<self.rows-1 else i),j ])
-                    temp[i,j] += self.policy[1] * (self.reward + \
-                        self.gamma * self.grid[ (i-1 if i>0 else i),j ])
-                    temp[i,j] += self.policy[2] * (self.reward + \
-                        self.gamma * self.grid[ i,(j+1 if j<self.cols-1 else j) ])
-                    temp[i,j] += self.policy[3] * (self.reward + \
-                        self.gamma * self.grid[ i,(j-1 if j>0 else j) ])
-        self.grid = temp
-    
+                    temp_grid[ i,j ] += self.policy[0] * (self.reward + self.gamma * self.grid[ (i+1 if i<self.rows-1 else i),j ])
+                    temp_grid[ i,j ] += self.policy[0] * (self.reward + self.gamma * self.grid[ (i-1 if i>0 else i),j ])
+                    temp_grid[ i,j ] += self.policy[0] * (self.reward + self.gamma * self.grid[ i,(j+1 if j<self.cols-1 else j) ])
+                    temp_grid[ i,j ] += self.policy[0] * (self.reward + self.gamma * self.grid[ i,(j-1 if j>0 else j) ])
+
+        self.grid = temp_grid
+
+
     def generate_obstacle(self):
         """ add an obstacle """
         obstacle = np.random.randint(min(self.rows,self.cols), size=(2,2))
         for i in range(obstacle[0,:]):
-          for j in range(obstacle[1,:]):
-            self.grid[i,j] = -50 ## <-- the obstacle value
+            for j in range(obstacle[1,:]):
+                self.grid[i,j] = -50 ## <-- the obstacle value
 
-    def get_path(self):
+    def find_path(self):
         """
           find path from start to end
         """
+        self.grid2=self.grid
+        for y in range(1,self.cols):
+            self.grid2[0,y]+=self.grid2[0,y-1]
+        for x in range(1,self.rows):
+            self.grid2[x,0]+=self.grid2[x-1,0]
+        for x in range(1,self.rows):
+            for y in range(1,self.cols):
+                self.grid2[x,y] += \
+                        max(self.grid2[x-1,y],self.grid2[x,y-1])
+
+        print(self.grid2)
         x,y=0,0
-        loc=(0,0)
-        self.path=[loc]
-        while(x!=self.rows-1 and y!=self.cols-1):
-          mx=-1*1e8
-          if(x-1>=0):
-            mx = self.grid[x-1,y] if self.gird[x-1,y]>mx else mx
-            loc=(x-1,y)
-          if(x+1<self.rows):
-            mx = self.grid[x+1,y] if self.gird[x+1,y]>mx else mx
-            loc=(x+1,y)
-          if(y-1>=self.cols):
-            mx = self.grid[x,y-1] if self.gird[x,y-1]>mx else mx
-            loc=(x,y-1)
-          if(y+1<self.cols):
-            mx = self.grid[x,y+1] if self.gird[x,y+1]>mx else mx
-            loc=(x,y+1)
-          self.path.append(loc) 
-              
+        self.path=[(x,y)]
+
+        while(x!=self.rows-1 or y!=self.cols-1):
+            mx=-1*1e6
+            (x2,y2) = (x,y)
+            if x+1<self.rows:
+                print("one")
+                if self.grid[x+1,y] > mx:
+                    print("two")
+                    (x2,y2) = (x+1,y)
+                    mx = self.grid[x2,y2]                    
+
+            if y+1<self.cols:
+                if self.grid[x,y+1] > mx:
+                    (x2,y2) = (x,y+1)
+                    mx = self.grid[x2,y2]                    
+
+            (x,y) = (x2,y2)
+            print("mx: "+str(mx))
+
+            print("current point: "+str((x,y)))
+            self.path.append((x,y)) 
+            print("current path: "+str(self.path))
+            print()
+
+    def show_path(self):
+        print(self.path)
+
+    def draw_path(self):
+        for point in self.path:
+            self.grid[point[0],point[1]]=0
+
     def show_array(self):
-        """ print the grid """
         print(self.grid)
 
     def show_heatmap(self):
-      pass
-
-
+        plt.imshow(self.grid, cmap=plt.cm.bwr)
+        plt.show()
 
 """
-  
+
 """
 obj = GWRL(4,4,[ 0.25, 0.25, 0.25, 0.25 ],-1,1)
-for i in range(10):
+for i in range(1000):
     obj.train()
-    obj.show()
+    obj.show_array()
     print()
 
+obj.find_path()
+#obj.show_array()
+#obj.draw_path()
+obj.show_heatmap()
+obj.show_path()
 
